30f88a96060f62f26fa803e89ac99dd269cd990c
clecyo.freitas <clecyo.freitas@dpu.def.br>
2022-05-30 20:16:58 -0300

    criação de componetes como input, button, form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
@@ -12,8 +12,8 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,9 @@
+
 import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
-  return (
-  <div className={styles.footer}>
-      Footer
-      </div>
-);
+  return <div className={styles.footer}>Footer</div>;
 };
 
 export default Footer;
