51e05bba5a59163739135358ce8c035b702b2b23
clecyo.freitas <clecyo.freitas@dpu.def.br>
2022-05-31 17:52:52 -0300

    inicio do modulo do usuario

---
+++ src/Components/Helper/ProtectedRoute.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom';
+import { UserContext } from '../../UserContext'
+
+const ProtectedRoute = ({ children }) => {
+    const { login } = React.useContext(UserContext); // recuperar os dados do login do usuario
+    return login ? children : <Navigate to="/login" />; // caso esteja logado irá retireciona para página especifica.
+}
+
+export default ProtectedRoute
