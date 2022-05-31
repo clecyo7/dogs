5ed2db0c9cd72462708f99177b27ad3a2ed5423c
clecyo.freitas <clecyo.freitas@dpu.def.br>
2022-05-31 14:10:12 -0300

    criação de formulario login e create user com css

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
+import { UserContext } from '../../UserContext';
+import styles from './Login.module.css';
 
 const Login = () => {
+
+  // caso o usuario esteja logado fazer o direcionamento para a página conya
+  const {login} = React.useContext(UserContext);
+  if(login ===true) return <Navigate to="/conta" />
+
+
   return (
-    <div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="criar" element={<LoginCreate />} />
@@ -15,6 +24,9 @@ const Login = () => {
         <Route path="resetar" element={<LoginPasswordReset />} />
       </Routes>
     </div>
+
+    </section>
+    
   );
 };
 
