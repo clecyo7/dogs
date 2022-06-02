1fe8bf95689829f602fed6000bc47723bf70c9f0
clecyo.freitas <clecyo.freitas@dpu.def.br>
2022-06-02 14:25:45 -0300

    melhoria do foto comentarios, feed fotos, loading

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -5,11 +5,17 @@ import styles from './PhotoComments.module.css';
 
 const PhotoComments = (props) => {
   const [comments, setComments] = React.useState(() => props.comments);
+  const commentsSection = React.useRef(null); //useRef retorna um objeto ref mutável, no qual a propriedade .current é inicializada para o argumento passado (initialValue). O objeto retornado persistirá durante todo o ciclo de vida do componente.
   const { login } = React.useContext(UserContext);
+
+  //jogar o scroll dos comentarios sempre na parte de baixo quando houve um novo comentário.
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
 
   return (
     <>
-      <ul className={styles.comments}>
+      <ul ref={commentsSection} className={styles.comments}>
         {comments.map((comment) => (
           <li key={comment.comment_ID}>
             <b>{comment.comment_author}: </b>
